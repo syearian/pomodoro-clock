062c2ee02c3a2a67d929e12a79f7d8eae62058ab
Steven Yearian <syearian@gmail.com>
2016-07-21 16:05:23 -0500

    Add global vars

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var ProgressBar = require('progressbar.js');
+
+var sessionTime = 25;
+var sessionMil = sessionTime * 60000;
+var breakTime = 5;
+var breakMil = breakTime * 60000;
+var timer = sessionTime + ':00';
+
+var sessionCircle = new ProgressBar.Circle(timerProgress, {
+  strokeWidth: 6,
+  duration: sessionMil,
+  from: { color: '#FFB300 '},
+  to: { color: '#43A047 '},
+  step: function(state, bar, attachment) {
+    bar.path.setAttribute('stroke', state.color);
+  },
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: null
+});
+var breakCircle = new ProgressBar.Circle(timerProgress, {
+  strokeWidth: 6,
+  duration: breakMil,
+  from: { color: '#43A047 '},
+  to: { color: '#FFB300 '},
+  step: function(state, bar, attachment) {
+    bar.path.setAttribute('stroke', state.color);
+  },
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: null
+});
